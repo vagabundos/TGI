4eb9184464da2e670ff05d266b155ca1bf9b8950
vagabundos <jonatasbazzoli@hotmail.com>
2015-03-31 21:35:28 -0300

    Controle de Abas na tela principal da aplicação
    Possibilidade de fechar abas abertas

--- GerenciadorDomotico/GerenciadorDomotico/winInicial.Designer.cs
+++ GerenciadorDomotico/GerenciadorDomotico/winInicial.Designer.cs
@@ -28,52 +28,57 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.stpMenu = new System.Windows.Forms.MenuStrip();
-			this.pnlGeral = new System.Windows.Forms.Panel();
-			this.tabCtrl1 = new System.Windows.Forms.TabControl();
-			this.pnlGeral.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// stpMenu
-			// 
-			this.stpMenu.Location = new System.Drawing.Point(0, 0);
-			this.stpMenu.Name = "stpMenu";
-			this.stpMenu.Size = new System.Drawing.Size(784, 24);
-			this.stpMenu.TabIndex = 0;
-			this.stpMenu.Text = "menuStrip1";
-			// 
-			// pnlGeral
-			// 
-			this.pnlGeral.Controls.Add(this.tabCtrl1);
-			this.pnlGeral.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.pnlGeral.Location = new System.Drawing.Point(0, 24);
-			this.pnlGeral.Name = "pnlGeral";
-			this.pnlGeral.Size = new System.Drawing.Size(784, 538);
-			this.pnlGeral.TabIndex = 1;
-			// 
-			// tabCtrl1
-			// 
-			this.tabCtrl1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tabCtrl1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-			this.tabCtrl1.Location = new System.Drawing.Point(0, 0);
-			this.tabCtrl1.Name = "tabCtrl1";
-			this.tabCtrl1.SelectedIndex = 0;
-			this.tabCtrl1.Size = new System.Drawing.Size(784, 538);
-			this.tabCtrl1.TabIndex = 1;
-			// 
-			// winInicial
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(784, 562);
-			this.Controls.Add(this.pnlGeral);
-			this.Controls.Add(this.stpMenu);
-			this.MainMenuStrip = this.stpMenu;
-			this.Name = "winInicial";
-			this.Text = "Gerenciador Domótico";
-			this.pnlGeral.ResumeLayout(false);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.stpMenu = new System.Windows.Forms.MenuStrip();
+            this.pnlGeral = new System.Windows.Forms.Panel();
+            this.tabCtrl1 = new System.Windows.Forms.TabControl();
+            this.pnlGeral.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // stpMenu
+            // 
+            this.stpMenu.Location = new System.Drawing.Point(0, 0);
+            this.stpMenu.Name = "stpMenu";
+            this.stpMenu.Size = new System.Drawing.Size(784, 24);
+            this.stpMenu.TabIndex = 0;
+            this.stpMenu.Text = "menuStrip1";
+            // 
+            // pnlGeral
+            // 
+            this.pnlGeral.Controls.Add(this.tabCtrl1);
+            this.pnlGeral.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlGeral.Location = new System.Drawing.Point(0, 24);
+            this.pnlGeral.Name = "pnlGeral";
+            this.pnlGeral.Size = new System.Drawing.Size(784, 538);
+            this.pnlGeral.TabIndex = 1;
+            // 
+            // tabCtrl1
+            // 
+            this.tabCtrl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabCtrl1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.tabCtrl1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.tabCtrl1.ItemSize = new System.Drawing.Size(120, 20);
+            this.tabCtrl1.Location = new System.Drawing.Point(0, 0);
+            this.tabCtrl1.Name = "tabCtrl1";
+            this.tabCtrl1.SelectedIndex = 0;
+            this.tabCtrl1.Size = new System.Drawing.Size(784, 538);
+            this.tabCtrl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabCtrl1.TabIndex = 1;
+            this.tabCtrl1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.tabCtrl1_DrawItem);
+            this.tabCtrl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.tabCtrl1_MouseDown);
+            // 
+            // winInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.pnlGeral);
+            this.Controls.Add(this.stpMenu);
+            this.MainMenuStrip = this.stpMenu;
+            this.Name = "winInicial";
+            this.Text = "Gerenciador Domótico";
+            this.pnlGeral.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -81,7 +86,7 @@
 
 		private System.Windows.Forms.MenuStrip stpMenu;
 		private System.Windows.Forms.Panel pnlGeral;
-		private System.Windows.Forms.TabControl tabCtrl1;
+        private System.Windows.Forms.TabControl tabCtrl1;
 	}
 }
 
--- GerenciadorDomotico/GerenciadorDomotico/winInicial.cs
+++ GerenciadorDomotico/GerenciadorDomotico/winInicial.cs
@@ -15,17 +15,26 @@ namespace GerenciadorDomotico
 		public winInicial()
 		{
 			InitializeComponent();
+
+            //stpMenu.ForeColor = System.Drawing.Color.White;
+            //stpMenu.BackColor = System.Drawing.Color.Black;
+            ConfiguraMenu(stpMenu);
+
 			ToolStripMenuItem objMenuItem;
 			ToolStripMenuItem objSubMenuItem;
 
 			#region Menu Configurações
 			objMenuItem = new ToolStripMenuItem("Configurações");
+            ConfiguraSubMenu(objMenuItem);
 			stpMenu.Items.Add(objMenuItem);
 
 			// Submenus
 			objSubMenuItem = new ToolStripMenuItem("Usuários", null, mniRotina_Click);
+            ConfiguraSubMenu(objSubMenuItem);
 			objMenuItem.DropDown.Items.Add(objSubMenuItem);
+
 			objSubMenuItem = new ToolStripMenuItem("Cômodos", null, mniRotina_Click);
+            ConfiguraSubMenu(objSubMenuItem);
 			objMenuItem.DropDown.Items.Add(objSubMenuItem);
 			#endregion
 
@@ -33,17 +42,66 @@ namespace GerenciadorDomotico
 			//porta.Open();
 		}
 
-		private void mniRotina_Click(object sender, EventArgs e)
+        #region Métodos
+
+        private void ConfiguraMenu(Control ctrl)
+        {
+            ctrl.ForeColor = System.Drawing.Color.White;
+            ctrl.BackColor = System.Drawing.Color.DarkSeaGreen;
+            ctrl.Font = new Font(ctrl.Font, FontStyle.Bold);
+        }
+
+        private void ConfiguraSubMenu(ToolStripItem ctrl)
+        {
+            ctrl.ForeColor = System.Drawing.Color.White;
+            ctrl.BackColor = System.Drawing.Color.DarkSeaGreen;
+            ctrl.Font = new Font(ctrl.Font, FontStyle.Bold);
+        }
+
+        #endregion
+
+        #region Eventos
+
+        private void mniRotina_Click(object sender, EventArgs e)
 		{
 			ToolStripMenuItem objMenuItem = (ToolStripMenuItem)sender;
 			if (!tabCtrl1.TabPages.ContainsKey(objMenuItem.Text))
 			{
 				tabCtrl1.TabPages.Add(objMenuItem.Text, objMenuItem.Text);
-				ctlBase ctrlCarregado = new ctlBase();
-				tabCtrl1.TabPages[objMenuItem.Text].Controls.Add(ctrlCarregado);
+                TabPage objTabPage = tabCtrl1.TabPages[objMenuItem.Text];
+                ctlBase ctrlCarregado = new ctlBase();
+                objTabPage.Controls.Add(ctrlCarregado);
 				this.ActiveControl = ctrlCarregado;
 				this.ActiveControl.Focus();
 			}
 		}
-	}
+
+        private void tabCtrl1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.Graphics.DrawString("x", e.Font, Brushes.Black, e.Bounds.Right - 15, e.Bounds.Top + 4);
+            e.Graphics.DrawString(this.tabCtrl1.TabPages[e.Index].Text, e.Font, Brushes.Black, e.Bounds.Left + 12, e.Bounds.Top + 4);
+            e.DrawFocusRectangle();
+        }
+
+        private void tabCtrl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Looping through the controls.
+            for (int i = 0; i < this.tabCtrl1.TabPages.Count; i++)
+            {
+                Rectangle r = this.tabCtrl1.GetTabRect(i);
+                //Getting the position of the "x" mark.
+                Rectangle closeButton = new Rectangle(r.Right - 15, r.Top + 4, 9, 7);
+                if (closeButton.Contains(e.Location))
+                {
+                    if (MessageBox.Show("Tem certeza que deseja fechar esta janela?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        this.tabCtrl1.TabPages.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
 }
